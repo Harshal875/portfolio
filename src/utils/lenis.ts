49caf0ec1048e4a108a0227a59b5669cf11dae94
Harshal Bansal <harshal.bansal@cvent.com>
2026-05-23 00:10:17 +0530

    fix: switch Lenis from duration-based to lerp: 0.1 for native-feeling scroll

--- src/utils/lenis.ts
+++ src/utils/lenis.ts
@@ -5,8 +5,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 const lenis = new Lenis({
-  duration: 0.7,
-  easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  lerp: 0.1,
   orientation: 'vertical',
   smoothWheel: true,
 })
